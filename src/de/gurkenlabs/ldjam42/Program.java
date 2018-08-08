9c49b0e602cdcd10d07ffde3561960b369fa90b1
Steffen Wilke <mail@steffen-wilke.de>
2018-08-09 01:08:02 +0200

    Initial commit of the LDJAM 42 Game project.
    - Init Gradle project
    - Created an empty game project
    - Added the LITIengine as submodule
    - Added README.md files for the (currently) empty resource folders
    - Added a gradle file to build everything and also distribute the game via launch4j
    - Added the JRE that will be shipped with the executable such that the game doesn't require any installed java

---
+++ src/de/gurkenlabs/ldjam42/Program.java
@@ -0,0 +1,20 @@
+package de.gurkenlabs.ldjam42;
+
+import de.gurkenlabs.litiengine.Game;
+
+/**
+ * @author Steffen Wilke
+ *
+ */
+public class Program {
+
+  /**
+   * The main entry point for the
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+    Game.init(args);
+    Game.start();
+  }
+}
