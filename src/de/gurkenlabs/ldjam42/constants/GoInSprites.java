fca356e4832e70b484839103284153b6c14a2bd1
nightm4re94 <matthias@gurkenlabs.de>
2018-08-11 23:48:10 +0200

    Add SpritePermutator to generate all possible permutations of given sprites.

---
+++ src/de/gurkenlabs/ldjam42/constants/GoInSprites.java
@@ -0,0 +1,28 @@
+package de.gurkenlabs.ldjam42.constants;
+
+import de.gurkenlabs.litiengine.graphics.Spritesheet;
+
+public final class GoInSprites {
+
+  public static final Spritesheet IDLE_DOWN_PANTS0 = Spritesheet.load("man_idle_down_pants0.png", 20, 22);
+  public static final Spritesheet IDLE_DOWN_TOP0 = Spritesheet.load("man_idle_down_top0.png", 20, 22);
+  public static final Spritesheet IDLE_DOWN_HAIR0 = Spritesheet.load("man_idle_down_hair0.png", 20, 22);
+  public static final Spritesheet IDLE_DOWN_FACE0 = Spritesheet.load("man_idle_down_face0.png", 20, 22);
+
+  public static final Spritesheet IDLE_DOWN_PANTS1 = Spritesheet.load("man_idle_down_pants1.png", 20, 22);
+  public static final Spritesheet IDLE_DOWN_TOP1 = Spritesheet.load("man_idle_down_top1.png", 20, 22);
+  public static final Spritesheet IDLE_DOWN_HAIR1 = Spritesheet.load("man_idle_down_hair1.png", 20, 22);
+  public static final Spritesheet IDLE_DOWN_FACE1 = Spritesheet.load("man_idle_down_face1.png", 20, 22);
+
+  public static final Spritesheet IDLE_DOWN_PANTS2 = Spritesheet.load("man_idle_down_pants2.png", 20, 22);
+  public static final Spritesheet IDLE_DOWN_TOP2 = Spritesheet.load("man_idle_down_top2.png", 20, 22);
+  public static final Spritesheet IDLE_DOWN_HAIR2 = Spritesheet.load("man_idle_down_hair2.png", 20, 22);
+  public static final Spritesheet IDLE_DOWN_FACE2 = Spritesheet.load("man_idle_down_face2.png", 20, 22);
+
+  public static final Spritesheet IDLE_DOWN_PANTS3 = Spritesheet.load("man_idle_down_pants3.png", 20, 22);
+  public static final Spritesheet IDLE_DOWN_TOP3 = Spritesheet.load("man_idle_down_top3.png", 20, 22);
+  public static final Spritesheet IDLE_DOWN_HAIR3 = Spritesheet.load("man_idle_down_hair3.png", 20, 22);
+  public static final Spritesheet IDLE_DOWN_FACE3 = Spritesheet.load("man_idle_down_face3.png", 20, 22);
+
+  public static final Spritesheet IDLE_DOWN = Spritesheet.load("man_idle_down.png", 20, 22);
+}
--- src/de/gurkenlabs/ldjam42/gui/MenuScreen.java
+++ src/de/gurkenlabs/ldjam42/gui/MenuScreen.java
@@ -1,17 +1,25 @@
 package de.gurkenlabs.ldjam42.gui;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.util.List;
 
 import de.gurkenlabs.ldjam42.GameManager;
+import de.gurkenlabs.ldjam42.constants.GoInSprites;
+import de.gurkenlabs.ldjam42.util.SpritePermutator;
 import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.graphics.FreeFlightCamera;
 import de.gurkenlabs.litiengine.gui.ImageComponent;
 import de.gurkenlabs.litiengine.gui.screens.Screen;
+import de.gurkenlabs.litiengine.util.ImageProcessing;
 
 public class MenuScreen extends Screen {
   public static final String NAME = "MENU";
   private ImageComponent playButton;
-
+  private SpritePermutator perm;
+  private Image[][] features;
   private boolean locked;
 
   public MenuScreen() {
@@ -22,6 +30,21 @@ public class MenuScreen extends Screen {
   public void render(final Graphics2D g) {
     if (Game.getEnvironment() != null) {
       Game.getEnvironment().render(g);
+    }
+    g.drawImage(ImageProcessing.scaleImage((BufferedImage) this.perm.getSpritePermutationsInOneImage(), 3.0f), 200, 0, null);
+    int testX = 500;
+    int testY = 50;
+    float testScale = 3.0f;
+    for (int typeIndex = 0; typeIndex < this.features.length; typeIndex++) {
+      for (int featureIndex = 0; featureIndex < this.features[typeIndex].length; featureIndex++) {
+        BufferedImage img = (BufferedImage) this.features[typeIndex][featureIndex];
+        int x = (int) (testX + featureIndex * img.getWidth() * testScale);
+        int y = (int) (testY + typeIndex * img.getHeight() * testScale);
+        g.drawImage(ImageProcessing.scaleImage(img, testScale), x, y, null);
+        g.setColor(Color.WHITE);
+        g.drawRect(x, y, (int) (img.getWidth() * testScale), (int) (img.getHeight() * testScale));
+        g.drawString(typeIndex + " - " + featureIndex, x + 20, y + img.getHeight() * testScale);
+      }
     }
 
     super.render(g);
@@ -55,6 +78,12 @@ public class MenuScreen extends Screen {
     });
 
     this.getComponents().add(this.playButton);
+
+    this.features = new Image[][] { { GoInSprites.IDLE_DOWN_PANTS0.getImage(), GoInSprites.IDLE_DOWN_PANTS1.getImage(), GoInSprites.IDLE_DOWN_PANTS2.getImage(), GoInSprites.IDLE_DOWN_PANTS3.getImage() },
+        { GoInSprites.IDLE_DOWN_TOP0.getImage(), GoInSprites.IDLE_DOWN_TOP1.getImage(), GoInSprites.IDLE_DOWN_TOP2.getImage(), GoInSprites.IDLE_DOWN_TOP3.getImage() },
+        { GoInSprites.IDLE_DOWN_FACE0.getImage(), GoInSprites.IDLE_DOWN_FACE1.getImage(), GoInSprites.IDLE_DOWN_FACE2.getImage(), GoInSprites.IDLE_DOWN_FACE3.getImage() },
+        { GoInSprites.IDLE_DOWN_HAIR0.getImage(), GoInSprites.IDLE_DOWN_HAIR1.getImage(), GoInSprites.IDLE_DOWN_HAIR2.getImage(), GoInSprites.IDLE_DOWN_HAIR3.getImage() } };
+    this.perm = new SpritePermutator(features);
   }
 
   private static void displayIngameScreen() {
