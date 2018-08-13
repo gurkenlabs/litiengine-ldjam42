aa74c9c399c064d74a698f16e4a57add2fa34e28
nightm4re94 <matthias@gurkenlabs.de>
2018-08-13 04:47:54 +0200

    Add actual club map.

---
+++ maps/tiles-club.tsx
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles-club" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="tiles-club.png" trans="ff00ff" width="256" height="256"/>
+ <terraintypes>
+  <terrain name="Stains" tile="0"/>
+ </terraintypes>
+ <tile id="96" terrain=",,,0"/>
+ <tile id="97" terrain=",,0,0"/>
+ <tile id="98" terrain=",,0,0"/>
+ <tile id="99" terrain=",,0,"/>
+ <tile id="112" terrain=",0,,0"/>
+ <tile id="113" terrain="0,0,0,"/>
+ <tile id="114" terrain="0,0,,0"/>
+ <tile id="115" terrain="0,,0,"/>
+ <tile id="116" terrain=",,,0"/>
+ <tile id="117" terrain=",,0,"/>
+ <tile id="128" terrain=",0,,0"/>
+ <tile id="129" terrain="0,,0,0"/>
+ <tile id="130" terrain=",0,0,0"/>
+ <tile id="131" terrain="0,,0,"/>
+ <tile id="132" terrain=",0,,"/>
+ <tile id="133" terrain="0,,,"/>
+ <tile id="144" terrain=",0,,"/>
+ <tile id="145" terrain="0,0,,"/>
+ <tile id="146" terrain="0,0,,"/>
+ <tile id="147" terrain="0,,,"/>
+</tileset>
